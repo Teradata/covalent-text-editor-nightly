ead43b868a9abb3a7c98afbf4f8c90f90f5f2b70
jeremysmartt <jeremy.smartt@teradata.com>
2017-08-10 17:29:25 -0700

    Removing ViewEncapsulation.None

--- src/platform/text-editor/text-editor.component.ts
+++ src/platform/text-editor/text-editor.component.ts
@@ -14,7 +14,6 @@ const noop: any = () => {
 
 @Component({
   selector: 'td-text-editor',
-  encapsulation: ViewEncapsulation.None,
   templateUrl: './text-editor.component.html',
   styleUrls: [ './text-editor.component.scss' ],
   providers: [{
